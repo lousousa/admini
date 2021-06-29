a531a7b837e409c55dd7d609761d608b5be2aa3d
wizard <rumpertumpski@gmail.com>
2021-06-28 21:58:39 -0300

    update project title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
   },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'theme-admin',
+    title: 'theme-admini',
     htmlAttrs: {
       lang: 'en'
     },
